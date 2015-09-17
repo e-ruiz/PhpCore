41c8cce55eb2a06ec2f76e0df0b210d4a1c6a441
Eric S. Lucinger Ruiz <eu@ericruiz.com.br>
2015-09-16 21:52:12 -0300

    many fixes and improvements, check wiki

--- Http/Request.php
+++ Http/Request.php
@@ -3,315 +3,162 @@
  * Http Request class file
  *
  * @author Eric S. Lucinger Ruiz <eu@ericruiz.com.br>
- * @version Sun 12 Jan 2014 14:27:40 BRST 
+ * @version Sun 12 Jan 2014 16:10:32 BRST 
  * @license http://opensource.org/licenses/MIT
  */
-
+ 
+ 
 namespace Core\Http;
+
 
 /**
  * Http Request
  *
+ * An abstraction of incoming request 
+ *
+ * @todo parse and support multipart/form-data
+ *
  * @package Core\Http
  * @author Eric S. Lucinger Ruiz <eu@ericruiz.com.br>
- * @version Sun 12 Jan 2014 14:27:40 BRST 
  */
 class Request
 {
-    /**
-     * @var self instance
-     */
-    private static $_instance;
-    
-    /**
-     * @var array valid http methods list
-     */
-    public static $validMethods = array(
-            'GET','POST','PUT','PATCH','DELETE','HEAD','OPTIONS', 'TRACE', 'CONNECT');
-    
-    /**
-     * @var Http\RequestUrl URL object
-     */
-    public $url;
-    
-    /**
-     * @var string Http Request body 
-     */
-    public $body;
-    
-    /**
-     * @var array cookies list
-     */
-    public $cookies;
-    
-    /**
-     * Singleton constructor
-     *
-     * @access private
-     */
-    private function __construct()
-    {
-        $this->method = $this->getMethod();
-        
-        // setup url
-        $this->url = new Url();
-        
-        // map get
-        $this->_mapHeaders();
-        
-        // map body, (!) alpha version 
-        $this->_mapBody();
-        
-        // map cookies
-        $this->cookies = $this->cookie();
-    }
-    
-    /**
-     * Singleton pattern, so you can not clone it
-     *
-     */
-    private function __clone() {}
-    
-    /**
-     * Singleton pattern 
-     *
-     * @return \Core\Http\Request
-     */
-    public static function getInstance()
-    {
-        if (!self::$_instance instanceof self)
-            self::$_instance = new self();
-        
-        return self::$_instance;
-    }
-    
-    /**
-     * $_SERVER alias
-     *
-     * @param string $var
-     * @return mixed
-     */
-    public static function server($var = null)
-    {
-        if ($var !== null)
-            return (isset($_SERVER[$var]))
-                ? $_SERVER[$var]
-                : null;
-        
-        return $_SERVER;
-    }
-    
-    /**
-     * $_REQUEST alias
-     *
-     * @param string $var
-     * @return mixed
-     */
-    public static function request($var = null)
-    {
-        if ($var !== null)
-            return (isset($_REQUEST[$var]))
-                ? $_REQUEST[$var]
-                : null;
-        
-        return $_REQUEST;
-    }
-    
-    /**
-     * $_GET alias
-     *
-     * @param string $var
-     * @return mixed
-     */
-    public static function get($var = null)
-    {
-        if ($var !== null)
-            return (isset($_GET[$var]))
-                ? $_GET[$var]
-                : null;
-        
-        return $_GET;
-    }
-    
-    /**
-     * $_POST alias
-     *
-     * @param string $var
-     * @return mixed
-     */
-    public static function post($var = null)
-    {
-        if ($var !== null)
-            return (isset($_POST[$var]))
-                ? $_POST[$var]
-                : null;
-        
-        return $_POST;
-    }
-    
-    /**
-     * $_COOKIE alias
-     *
-     * @param string $var
-     * @return mixed
-     */
-    public function cookie($var = null)
-    {
-        if ($var !== null)
-            return (isset($_COOKIE[$var]))
-                ? $_COOKIE[$var]
-                : null;
-        
-        return $_COOKIE;
-    }
-    
-    /**
-     * Returns Http requested method
-     *
-     * Supports X-Http-Method-Override header and POST overhide under _METHOD field
-     *
-     * @todo refactor, check good practices
-     *
-     * @return string
-     */
-    public static function getMethod()
-    {
-        $method = strtoupper(self::server('REQUEST_METHOD'));
-        
-        // overrides all, return if method is valid, else returns null
-        if ($override = strtoupper(self::server('HTTP_X_HTTP_METHOD_OVERRIDE')))
-            return (in_array($override, self::$validMethods))
-                ? $override
-                : 'UNKNOW_METHOD: ' . $override;
-        
-        /*
-         * POST form override method --> POST x-www-form-urlencoded field:_METHOD=PUT
-         * Useful with browsers who do not support some http methods like PUT
-         */
-        if (strtoupper(self::server('REQUEST_METHOD')) == 'POST')
-            // overrides http method, return if method is valid, else returns null
-            if ($override = strtoupper(self::post('_METHOD')))
-                return (in_array($override, self::$validMethods))
-                    ? $override
-                    : 'UNKNOW_METHOD: ' . $override;
-        
-        // default http method discoverer
-        return (in_array($method, self::$validMethods))
-            ? $method
-            : 'UNKNOW_METHOD: ' . $method; // just in case;
-    }
-    
-    /**
-     * Maps http request body
-     *
-     * @todo REFACTOR! Alpha version!
-     *
-     * @see http://php.net/manual/en/function.http-get-request-body.php
-     * @see http://php.net/manual/en/reserved.variables.httprawpostdata.php
-     * @return void
-     */
-    private function _mapBody()
-    {
-        if ($this->method == 'GET' || $this->method == 'POST') {
-            $this->body = $this->request();
-            
-            // $this->body_raw = ??;
-            return;
-        }
-        
-        $this->body = null;
-        if ($body_raw = @file_get_contents('php://input')) {  
-        
-            foreach (explode('&', $body_raw) as $value) {
-            
-                $val = substr($value, strpos($value, '=') + 1);
-                $key = substr($value, 0, strpos($value, '='));
-                
-                $this->body[$key] = $val;
-            }
-        }
-    }
-    
-    /**
-     * Map request headers
-     *
-     * @return void
-     */
-    private function _mapHeaders()
-    {
-        foreach ($this->server() as $key => $value) {
-        
-            if (preg_match('~^(http)~i', $key)) {
-                
-                $key = preg_replace('~^(http_)~i', '', $key);
-                
-                $key = preg_replace('~(_)~', ' ', $key);
-                $key = ucwords(strtolower($key)) . '-';
-                $key = preg_replace('~( )~', '-', $key);
-                
-                $key = trim($key, '-');
-                $this->headers[$key] = $value;
-            }
-        }
-    }
-    
-    /**
-     * Map $_POST
-     *
-     * @todo refactor
-     * @return void
-     */
-    private function _mapPost()
-    {
-        foreach ($this->post() as $field => $value) {
-            
-            $this->post[$field] = $value;
-        }
-    }
-    
-    /**
-     * Map $_GET
-     *
-     * @todo refactor
-     * @return void
-     */
-    private function _mapGet()
-    {
-        foreach ($this->get() as $field => $value) {
-            
-            $this->get[$field] = $value;
-        }
-    }
-    
-    /**
-     * Returns client IP even behind proxies
-     *
-     * @todo REFACTOR! test with different servers than apache
-     */
-    public function getClientIp()
-    {
-        foreach (array('HTTP_CLIENT_IP',
-                        'HTTP_X_FORWARDED_FOR',
-                        'HTTP_X_FORWARDED',
-                        'HTTP_X_CLUSTER_CLIENT_IP',
-                        'HTTP_FORWARDED_FOR',
-                        'HTTP_FORWARDED',
-                        'REMOTE_ADDR') as $key) {
-            if (array_key_exists($key, $_SERVER) === true) {
-                foreach (explode(',', $_SERVER[$key]) as $IPaddress){
-                    $IPaddress = trim($IPaddress); // Just in case
+	/**
+	 * List of a valid methods
+	 * 
+	 * It would be a CONSTANT, but it forces to run on PHP 5.6
+	 * 
+	 * @see http://www.w3.org/Protocols/rfc2616/rfc2616-sec9.html#sec9
+	 * @var array
+	 */
+	public static  $methods = array(
+			'GET','POST','PUT','DELETE',
+			'OPTIONS','CONNECT','TRACE','HEAD');
+	
+	/**
+	 * @var string
+	 */
+	private $_method;
+	
+	/**
+	 * @var \Core\Http\Url
+	 */
+	private $_url;
+	
+	/**
+	 * @var string
+	 */
+	private $_body;
+	
+	/**
+	 * @var array
+	 */
+	private $_headers;
+	
+	/**
+	 * @var array
+	 */
+	private $_cookies;
+	
+	public function __construct()
+	{
+		$this->_method  = $this->getMethod();
+		$this->_url     = $this->getUrl();
+		$this->_body    = $this->getBody();
+		$this->_headers = $this->getHeaders();
+		$this->_cookies = $this->getCookies();
+	}
+	
+	/**
+	 * @todo refactor check if override is valid else raise exception
+	 */
+	public static function getMethod()
+	{
+		$override = (self::getHeaders('X-Http-Method-Override'))
+				  ? strtoupper(self::getHeaders('X-Http-Method-Override'))
+				  : null;
+	
+		$override = (in_array($override, self::$methods))
+				  ? $override
+				  : null;
+	
+		$method = strtoupper($_SERVER['REQUEST_METHOD']);
+	
+		return ($override) ? $override : $method;
+	}
 
-                    /*
-                    if (filter_var($IPaddress,
-                                   FILTER_VALIDATE_IP,
-                                   FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE)
-                        !== false) {
-                    */
+	public static function getUrl()
+	{
+		return new \Core\Http\Url();;
+	}
+		
+	public static function getBody()
+	{
+		$contentType = self::getHeaders('Content-Type');
+		$contentType = explode(';', preg_replace('~( )~', '', $contentType));
 
-                        return $IPaddress;
-                    //}
-                }
-            }
-        }
-    }
+		/*
+		 * @todo parse multpart form data, specially when method is PUT
+		 */
+		if (in_array('multipart/form-data', $contentType))
+			throw new \Exception('Unsupported Media Type', 415);
+
+		parse_str(file_get_contents("php://input"), $body);
+		
+		return $body;
+	}
+	
+	/**
+	 * @todo check RFC about case-insensitive headers names
+	 *
+	 * @param string $param
+	 * @return mixed
+	 */
+	public static function getHeaders($param = null)
+	{
+		$headers = array();
+		foreach ($_SERVER as $key => $value) {
+			if (preg_match('~^(http)~i', $key)) {
+	
+				// HTTP_REQUEST_HEADER becomes Request-Header
+				$key = preg_replace('~^(http_)~i', '', $key);
+				$key = preg_replace('~(_)~', ' ', $key);
+				$key = ucwords(strtolower($key)) . '-';
+				$key = preg_replace('~( )~', '-', $key);
+				$key = trim($key, '-');
+	
+				$headers[$key] = $value;
+			}
+		}
+
+		if ($param) {
+			if (isset($headers[$param]))
+				return $headers[$param];
+			else
+				return null;
+		}
+		
+		return $headers;
+	}
+	
+	public static function getCookies($param = null)
+	{
+		if ($param) {
+			if (isset($_COOKIE[$param]))
+				return $_COOKIE[$param];
+			else
+				return null;
+		}
+	
+		return $_COOKIE;
+	}
+	
+	/**
+	 * Check if $_SERVER['HTTP_X_REQUESTED_WITH'] === 'xmlhttprequest'
+	 */
+	public static function isAjax()
+	{
+		$ajax = strtolower(\Core\Http\Request::getHeaders('X-Requested-With'));
+		return ($ajax === 'xmlhttprequest');
+	}
 }
--- Http/Url.php
+++ Http/Url.php
@@ -6,9 +6,10 @@
  * @version Sun 12 Jan 2014 16:10:32 BRST 
  * @license http://opensource.org/licenses/MIT
  */
-
-
+ 
+ 
 namespace Core\Http;
+
 
 /**
  * Http Url
@@ -19,119 +20,170 @@ namespace Core\Http;
  */
 class Url
 {
-    private $_url = '';
-    
-    public $parsed = array (
-            'scheme' => null,
-            'host' => null,
-            'port' => null,
-            'user' => null,
-            'pass' => null,
-            'path' => null,
-            'query' => null,
-            'fragment' => null
-    );
-    
-    /**
-     * Setup a URL string, map and parse that
-     *
-     * @todo need more studies arround security and server (Apache, Nginx) enviroments
-     *
-     * @param string $url if is null, it will get current URL by $_SERVER enviroment
-     */
-    public function __construct($url = null)
-    {
-        if ($url === null) { 
-        
-            $this->_url  = (Request::server('HTTPS') == 'on') ? 'https://' : 'http://';
-            $this->_url .= Request::server('HTTP_HOST') . Request::server('REQUEST_URI');
-        
-        } else {
-        
-            $this->_url = $url;
-        }
-        
-        $this->_mapParsedUrl(parse_url($this->_url));
-        
-        if (!$this->parsed)
-            throw new Exception('Malformed URL!');
-    }
-    
-    /**
-     * Echoes a parsed URL
-     *
-     * @return string
-     */
-    public function __toString()
-    {
-        return $this->unparseUrl($this->parsed);
-    }
-    
-    /**
-     * If is trying to get some parsed param, return this param
-     *
-     * @param string $param
-     * @return mixed
-     */
-    public function __get($param)
-    {
-        if (array_key_exists($param, $this->parsed))
-            return $this->parsed[$param];
-    }
-    
-    /**
-     * If is trying to set some URL structure param, maps that on $_parsed
-     *
-     * @param string $param
-     * @return mixed
-     */
-    public function __set($param, $value)
-    {
-        if (array_key_exists($param, $this->parsed))
-            $this->parsed[$param] = $value;
-            
-        else
-            $this->$param = $value;
-    }
-    
-    /**
-     * Map parse_url keys into $this->parsed[]
-     *
-     * @param array $parsed_url
-     * @return void
-     */
-    private function _mapParsedUrl(array $parsed_url)
-    {
-        foreach ($parsed_url as $key => $value)
-            $this->parsed[$key] = $value;
-    }
-    
-    /**
-     * Unparse an array provided by parse_url()
-     *
-     * @author thomas at gielfeldt dot com (from php.net)
-     * @see http://php.net/manual/en/function.parse-url.php#106731
-     * @param array $parsed_url
-     * @return string
-     */
-    private function unparseUrl(array $parsed_url = null)
-    {
-        // some implementation
-        $parsed_url = ($parsed_url == null) ? $this->parsed : $parsed_url;
-        if (!$parsed_url)
-            throw new InvalidArgumentException('Invalid argument!', 500);
-            
-        // original code
-        $scheme   = isset($parsed_url['scheme']) ? $parsed_url['scheme'] . '://' : '';
-        $host     = isset($parsed_url['host']) ? $parsed_url['host'] : '';
-        $port     = isset($parsed_url['port']) ? ':' . $parsed_url['port'] : '';
-        $user     = isset($parsed_url['user']) ? $parsed_url['user'] : '';
-        $pass     = isset($parsed_url['pass']) ? ':' . $parsed_url['pass']  : '';
-        $pass     = ($user || $pass) ? "$pass@" : '';
-        $path     = isset($parsed_url['path']) ? $parsed_url['path'] : '';
-        $query    = isset($parsed_url['query']) ? '?' . $parsed_url['query'] : '';
-        $fragment = isset($parsed_url['fragment']) ? '#' . $parsed_url['fragment'] : '';
-        
-        return $scheme . $user . $pass . $host . $port . $path . $query . $fragment;
-    } 
+	/**
+	 * @var string
+	 */
+	private $_scheme = '';
+	
+	/**
+	 * @var string
+	 */
+	private $_user = '';
+	
+	/**
+	 * @var string
+	 */
+	private $_pass = '';
+	
+	/**
+	 * @var string
+	 */
+	private $_host = '';
+	
+	/**
+	 * @var string
+	 */
+	private $_port = '';
+	
+	/**
+	 * @var string
+	 */
+	private $_path = '';
+	
+	/**
+	 * @var string
+	 */
+	private $_query = '';
+	
+	/**
+	 * @var string
+	 */
+	private $_fragment = '';
+	
+	public function __construct($url = null)
+	{
+		if ($url == null) {
+		
+			$url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on')
+			     ? 'https://' : 'http://';
+			
+			$url .= $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+		}
+		
+		if (!$parsed = parse_url($url))
+			throw new Exception('Malformed URL!');
+		
+		$this->_scheme = parse_url($url, PHP_URL_SCHEME);
+		$this->_user = parse_url($url, PHP_URL_USER);
+		$this->_pass = parse_url($url, PHP_URL_PASS);
+		$this->_host = parse_url($url, PHP_URL_HOST);
+		$this->_port = parse_url($url, PHP_URL_PORT);
+		$this->_path = parse_url($url, PHP_URL_PATH);
+		$this->_query = parse_url($url, PHP_URL_QUERY);
+		$this->_fragment = parse_url($url, PHP_URL_FRAGMENT);
+	}
+	
+	/**
+	 * "Unparse" the URL
+	 * 
+	 * @return string
+	 */
+	public function __toString()
+	{
+		/*
+		 * @author thomas at gielfeldt dot com
+		 * @link http://php.net/manual/en/function.parse-url.php
+		 */
+		$scheme   = isset($this->_scheme)   ? $this->_scheme . '://' : '';
+		$host     = isset($this->_host)     ? $this->_host           : '';
+		$port     = isset($this->_port)     ? ':' . $this->_port     : '';
+		$user     = isset($this->_user)     ? $this->_user           : '';
+		$pass     = isset($this->_pass)     ? ':' . $this->_pass     : '';
+		$pass     = ($user || $pass)        ? "$pass@"               : '';
+		$path     = isset($this->_path)     ? $this->_path           : '';
+		$query    = isset($this->_query)    ? '?' . $this->_query    : '';
+		$fragment = isset($this->_fragment) ? '#' . $this->_fragment : '';
+		
+		return $scheme . $user . $pass . $host . $port . $path . $query . $fragment;
+	}
+	
+	/**
+	 * @return string|null
+	 */
+	public function getScheme()
+	{
+		return $this->_scheme;
+	}
+	
+	/**
+	 * @return string|null
+	 */
+	public function getUser()
+	{
+		return $this->_user;
+	}
+	
+	/**
+	 * @return string|null
+	 */
+	public function getPass()
+	{
+		return $this->_pass;
+	}
+	
+	/**
+	 * @return string|null
+	 */
+	public function getHost()
+	{
+		return $this->_host;
+	}
+	
+	/**
+	 * @return string|null
+	 */
+	public function getPort()
+	{
+		return $this->_port;
+	}
+	
+	/**
+	 * @return string|null
+	 */
+	public function getPath()
+	{
+		return $this->_path;
+	}
+	
+	/**
+	 * Return query string as an associative array
+	 * 
+	 * @param string $param param name
+	 * @return array|null
+	 */
+	public function getQuery($param = null)
+	{
+		$query = array();
+		foreach (explode('&', $this->_query) as $arg) {
+			$z = explode('=', $arg);
+			$query[$z[0]] = $z[1];
+		}
+		
+		// remember 0 (zero) is a valid index
+		if ($param === null)
+			return $query;
+		
+		if (array_key_exists($param, $query))
+			return $query[$param];
+		else
+			return null;
+	}
+	
+	/**
+	 * @return string|null
+	 */
+	public function getFragment()
+	{
+		return $this->_fragment;
+	}
 }
